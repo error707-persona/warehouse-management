d03231617bd38d5314eb32a1cb357fdf9345cdac
error707-persona <areeshasayed09@gmail.com>
2025-07-16 23:09:30 +0530

    changes seeding sequence

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -16,7 +16,7 @@ async function deleteAllData(orderedFileNames: string[]) {
   for (const modelName of modelNames) {
     const model: any = prisma[modelName as keyof typeof prisma];
     if (model) {
-      await model.deleteMany({});
+      // await model.deleteMany({});
       console.log(`Cleared data from ${modelName}`);
     } else {
       console.error(
@@ -30,15 +30,11 @@ async function main() {
   const dataDirectory = path.join(__dirname, "seedData");
 
   const orderedFileNames = [
-    "users.json", // Independent
-    "expenses.json", // Independent
-    "salesSummary.json", // Independent summary
-    "purchaseSummary.json", // Independent summary
-    "expenseByCategory.json", // Depends on ExpenseSummary
     "expenseSummary.json", // Parent of ExpenseByCategory
+    "expenseByCategory.json", // Depends on ExpenseSummary
+    "products.json", // Parent of Sales & Purchases
     "sales.json", // Depends on Products
     "purchases.json", // Depends on Products
-    "products.json", // Parent of Sales & Purchases
   ];
 
   await deleteAllData(orderedFileNames);
